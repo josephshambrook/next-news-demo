6b3d97addc785a7f822231fe9706660d05380432
Joe Shambrook <jshambrook@and.digital>
2022-06-25 17:21:24 +0100

    Implemented grid layout

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,29 +1,63 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Article as ArticleProps } from "../types";
+import classnames from "classnames";
+import { Article as ArticleType } from "../types";
 import styles from "./Grid.module.css";
 
-const Article = ({ title, image, id, ...props }: ArticleProps) => {
+type ArticleComponentProps = ArticleType & {
+  placementIndex: number;
+};
+
+const Article = ({
+  // destructure all known article props first
+  title,
+  image,
+  id,
+  author,
+  description,
+  url,
+  source,
+  category,
+  language,
+  country,
+  published_at,
+  placementIndex,
+  ...props
+}: ArticleComponentProps) => {
+  const displayImage = [3, 4, 5].includes(placementIndex);
+  const displayLargeImage = [3].includes(placementIndex);
+  const displayDescription = [1, 3, 6, 7].includes(placementIndex);
+
   return (
     <div className={styles.article} {...props}>
+      {image && displayImage && (
+        <div
+          className={classnames(styles["image-wrapper"], {
+            [styles["image-wrapper-lg"]]: displayLargeImage
+          })}
+        >
+          <Image
+            layout="fill"
+            src={image}
+            quality={100}
+            alt="article"
+            objectFit="cover"
+          />
+        </div>
+      )}
+
       <Link href={"/articles/" + id}>
         <a>
           <h3 className={styles.headline}>{title}</h3>
-
-          {image && (
-            <div className={styles["image-wrapper"]}>
-              <Image
-                layout="fill"
-                src={image}
-                quality={100}
-                alt="article"
-                objectFit="cover"
-              />
-            </div>
-          )}
         </a>
       </Link>
+
+      {description && displayDescription && (
+        <p className={styles.description}>{description}</p>
+      )}
+
+      {author && <p className={styles.author}>By {author}</p>}
     </div>
   );
 };
--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -1,47 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Article from "./Article";
 import styles from "./Grid.module.css";
+import { Article as ArticleType } from "../types";
+
+const chunkArray = (arr, size) =>
+  arr.length > size
+    ? [arr.slice(0, size), ...chunkArray(arr.slice(size), size)]
+    : [arr];
 
 const ArticleList = ({ articles }) => {
-  const ARTICLES_PER_PAGE = 14;
+  const ARTICLES_PER_ROW = 7;
+  const ROWS_PER_PAGE = 2;
 
-  const [posts, setPosts] = React.useState(
-    articles.slice(0, ARTICLES_PER_PAGE)
-  );
-  const [hasMore, setHasMore] = React.useState(true);
+  const [groupedArticles, setGroupedArticles] = useState([]);
 
-  const getMoreArticles = () => {
-    if (posts.length >= articles.length) {
-      setHasMore(false);
-      return;
-    }
+  useEffect(() => {
+    const res = chunkArray(articles, ARTICLES_PER_ROW);
+    console.log(res);
+    setGroupedArticles(res);
+  }, [articles]);
 
-    setPosts(
-      posts.concat(
-        articles.slice(posts.length, posts.length + ARTICLES_PER_PAGE)
-      )
-    );
-  };
+  // const [posts, setPosts] = useState(articles.slice(0, ROWS_PER_PAGE));
+  // const [hasMore, setHasMore] = useState(true);
 
-  return (
-    <InfiniteScroll
-      className={styles.container}
-      dataLength={posts.length}
-      next={getMoreArticles}
-      hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
-    >
-      {posts.map((article, index) => {
-        return <Article key={index} {...article} />;
+  // const getMoreArticles = () => {
+  //   if (posts.length >= articles.length) {
+  //     setHasMore(false);
+  //     return;
+  //   }
+
+  //   setPosts(
+  //     posts.concat(articles.slice(posts.length, posts.length + ROWS_PER_PAGE))
+  //   );
+  // };
+
+  return groupedArticles.map((posts) => (
+    <div className={styles.container}>
+      {posts.map((article: ArticleType, index: number) => {
+        const placementIndex = Math.floor(index % ARTICLES_PER_ROW) + 1;
+        return (
+          <Article key={index} placementIndex={placementIndex} {...article} />
+        );
       })}
-    </InfiniteScroll>
-  );
+    </div>
+  ));
+
+  // return (
+  //   <div className={styles.container}>
+  //     {posts.map((article: ArticleType, index: number) => {
+  //       const placementIndex = Math.floor(index % ARTICLES_PER_ROW) + 1;
+  //       return (
+  //         <Article key={index} placementIndex={placementIndex} {...article} />
+  //       );
+  //     })}
+  //   </div>
+  // );
+
+  // return (
+  //   <InfiniteScroll
+  //     className={styles.container}
+  //     dataLength={posts.length}
+  //     next={getMoreArticles}
+  //     hasMore={hasMore}
+  //     loader={<h4>Loading...</h4>}
+  //     endMessage={
+  //       <p style={{ textAlign: "center" }}>
+  //         <b>Yay! You have seen it all</b>
+  //       </p>
+  //     }
+  //   >
+  //     {posts.map((article, index) => {
+  //       return <Article key={index} {...article} />;
+  //     })}
+  //   </InfiniteScroll>
+  // );
 };
 
 export default ArticleList;
