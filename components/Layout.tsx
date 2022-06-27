10b0e2308ec74a2cc62dfd4610192279e921ca05
Joe Shambrook <jshambrook@and.digital>
2022-06-27 09:09:37 +0100

    Update gitignore, fix a couple of ts problems

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import layoutStyles from "./Layout.module.css";
 
-export function Layout(props) {
+type LayoutProps = {
+  children?: React.ReactNode;
+};
+
+export function Layout({ children }: LayoutProps) {
   return (
     <div className={layoutStyles.container}>
       <header className={layoutStyles.header}>Latest updates</header>
-      <main className={layoutStyles.content} {...props} />
+      <main className={layoutStyles.content}>{children}</main>
       <footer className={layoutStyles.footer}>Somerville 2022</footer>
     </div>
   );
