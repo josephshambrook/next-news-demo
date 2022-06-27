b6ecf765d8665477af4912ace25e9be82c9d27b8
Joe Shambrook <jshambrook@and.digital>
2022-06-27 11:05:56 +0100

    Implement SSG-friendly news fetching on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ArticleList from "../components/ArticleList";
+import getNews from "../lib/getNews";
 import type { Article as ArticleType } from "../types";
 
 export default function IndexPage({ articles }: { articles: ArticleType[] }) {
@@ -7,9 +8,16 @@ export default function IndexPage({ articles }: { articles: ArticleType[] }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/news");
+  // TIL: we previously tried to use fetch here to get the news articles
+  // but because this is run on the server and never on the client,
+  // we can (and should) import the server code directly and use it!
 
-  const articles = await res.json();
+  // so instead of this
+  // const res = await fetch("http://localhost:3000/api/news");
+  // const articles = await res.json();
+
+  // do this
+  const articles = getNews();
 
   return {
     props: {
